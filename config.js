ff1fc5c7569dffa0751bcd2778f0b97ffd8c32ea
Yehonal <yehonal.azeroth@gmail.com>
2016-12-02 15:34:37 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+/*jslint browser: true, white: true, plusplus: true*/
+/*global angular, console, alert*/
+
+(function () {
+  'use strict';
+  var app = angular.module('arenastats');
+
+  /* Edit with path of TC-JSON-API */
+  app.api = "http://azerothshard.org/modules/TC-JSON-API/public/index.php/";
+
+  app.serverName = "AzerothShard";
+
+}());
